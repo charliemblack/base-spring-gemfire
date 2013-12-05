7b42c42fbf9e9d5b8bfb5c3cac311c88b789ec45
Charlie Black <charliemblack@gmail.com>
2013-12-05 12:15:23 -0800

    Should be good for now

--- bin/start_gemfire.sh
+++ bin/start_gemfire.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 
+source `dirname $0`/env.sh
 
-HOSTS="smdw smdw2 smdw3"
-SSH_USERNAME=gpadmin
 
-machineNumber=1
+${TEST_CASE_HOME}/bin/start_locator.sh
 
-ssh ${SSH_USERNAME}@mdw  "cd /data/FINRA/poc/bin; ./start_locator.sh"
+${TEST_CASE_HOME}/bin/start_data_node.sh 1
 
-for curr_host in ${HOSTS}
-do
-  echo Starting $machineNumber
-  ssh ${SSH_USERNAME}@${curr_host}  "cd /data/FINRA/poc/bin; ./start_data_node.sh ${machineNumber}"
-  machineNumber=$[machineNumber+1]
-done
+${TEST_CASE_HOME}/bin/start_data_node.sh 2
 
-# There was some wierd thing up with java and being able to spawn two large JVMs concurrently.
-# Lets leave that for another day
-sleep 10
+${TEST_CASE_HOME}/bin/start_data_node.sh 3
 
-for curr_host in ${HOSTS}
-do
-  echo Starting $machineNumber
-  ssh ${SSH_USERNAME}@${curr_host}  "cd /data/FINRA/poc/bin; ./start_data_node.sh ${machineNumber}"
-  machineNumber=$[machineNumber+1]
-done
-
+wait
--- bin/stop_gemfire.sh
+++ bin/stop_gemfire.sh
@@ -4,6 +4,10 @@ TEST_CASE_HOME=`dirname $0`/..
 
 source $TEST_CASE_HOME/bin/env.sh
 
-gfsh -e "connect --locator=localhost[55221]" -e "shutdown --time-out=15"
+pushd  $TEST_CASE_HOME/bin/logs > /dev/null 2>&1
+
+gfsh -e "connect --locator=localhost[10334]" -e "list members" -e "shutdown --time-out=15" -e "list members"
 
 gfsh stop locator --dir=$TEST_CASE_HOME/data/locator
+
+popd > /dev/null 2>&1
